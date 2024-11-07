212d9d4f924ffd2ada11397b84b40aa0bb9e7052
Trương Hữu Thọ <96384986+Havold@users.noreply.github.com>
2024-11-07 20:23:31 +0700

    Add method GET all students.

--- controllers/student.js
+++ controllers/student.js
@@ -2,8 +2,22 @@ import prisma from "../client.js";
 
 export const getAllStudents = async (req, res) => {
   try {
-    const students = await prisma.student.findMany();
-    res.status(200).json(students);
+    const ITEMS_PER_PAGE = req.query.pageItems
+      ? parseInt(req.query.pageItems)
+      : 5;
+    const CURRENT_PAGE = req.query.page ? parseInt(req.query.page) : 1;
+    const [students, count] = await prisma.$transaction([
+      prisma.student.findMany({
+        include: {
+          grade: true,
+          class: true,
+        },
+        take: ITEMS_PER_PAGE,
+        skip: (CURRENT_PAGE - 1) * ITEMS_PER_PAGE,
+      }),
+      prisma.student.count(),
+    ]);
+    res.status(200).json({ students, totalCount: count });
   } catch (error) {
     res.status(500).json(error);
   }
