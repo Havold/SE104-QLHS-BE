9f41a42855049ef573f89ed742907e815dd3a2bc
Trương Hữu Thọ <96384986+Havold@users.noreply.github.com>
2024-11-07 17:35:48 +0700

    Add method GET all teachers.

--- controllers/teacher.js
+++ controllers/teacher.js
@@ -1,19 +1,26 @@
 import prisma from "../client.js";
 
 export const getAllTeachers = async (req, res) => {
+  const PAGE_ITEMS = parseInt(req.query.pageItems) || 10;
+  const CURRENT_PAGE = parseInt(req.query.page) || 1;
   try {
-    const teachers = await prisma.teacher.findMany({
-      include: {
-        class: true,
-        subjects: true,
-        lessons: {
-          include: {
-            class: true,
+    const [teachers, count] = await prisma.$transaction([
+      prisma.teacher.findMany({
+        include: {
+          class: true,
+          subjects: true,
+          lessons: {
+            include: {
+              class: true,
+            },
           },
         },
-      },
-    });
-    res.status(200).json(teachers);
+        take: PAGE_ITEMS,
+        skip: PAGE_ITEMS * (CURRENT_PAGE - 1),
+      }),
+      prisma.teacher.count(),
+    ]);
+    res.status(200).json({ teachers, totalCount: count });
   } catch (error) {
     res.status(500).json(error);
   }
