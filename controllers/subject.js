93d016bdd9415d4a52fb8946ebcc9dbd8fc36fb4
Trương Hữu Thọ <96384986+Havold@users.noreply.github.com>
2024-12-21 17:45:07 +0700

    Add PUT and DELETE method. Fix bug: page now will always be the last page if request > total items.

--- controllers/subject.js
+++ controllers/subject.js
@@ -2,10 +2,11 @@ import { skip } from "@prisma/client/runtime/library";
 import prisma from "../client.js";
 import jwt from "jsonwebtoken";
 
+// GET ALL SUBJECTS
 export const getAllSubjects = async (req, res) => {
   const { page, pageItems, ...queryParams } = req.query;
-  const p = page ? parseInt(page) : 1;
-  const pItems = pageItems ? parseInt(pageItems) : 5;
+  let p = page ? parseInt(page) : 1;
+  let pItems = pageItems ? parseInt(pageItems) : 5;
   let query = {};
   if (queryParams) {
     for (const [key, value] of Object.entries(queryParams)) {
@@ -23,22 +24,23 @@ export const getAllSubjects = async (req, res) => {
   }
 
   try {
-    const [subjects, count] = await prisma.$transaction([
-      prisma.subject.findMany({
-        where: query,
-        take: pItems,
-        skip: (p - 1) * pItems,
-      }),
-      prisma.subject.count(),
-    ]);
-    // const subjects = await prisma.subject.findMany();
-    res.status(200).json({ subjects, totalCount: count });
+    const count = await prisma.subject.count();
+    if (p * pItems > count) {
+      p = Math.ceil(count / pItems);
+    }
+    const subjects = await prisma.subject.findMany({
+      where: query,
+      take: pItems,
+      skip: (p - 1) * pItems,
+    });
+    res.status(200).json({ subjects, totalCount: count, currentPage: p });
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
   }
 };
 
+// GET A SUBJECT
 export const getSubject = async (req, res) => {
   const subjectId = parseInt(req.params.id);
   try {
@@ -83,5 +85,74 @@ export const addSubject = (req, res) => {
       },
     });
     return res.status(200).json("New subject has been added!");
+  });
+};
+
+// METHOD DELETE
+export const deleteSubject = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) {
+    return res.status(401).json("YOU ARE NOT LOGGED IN!");
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, async (err, userInfo) => {
+    if (err) {
+      return res.status(403).json("INVALID TOKEN!");
+    }
+
+    const subjectId = parseInt(req.params.id);
+
+    const subject = await prisma.subject.findUnique({
+      select: {
+        name: true,
+      },
+      where: {
+        id: subjectId,
+      },
+    });
+    await prisma.subject.delete({
+      where: {
+        id: subjectId,
+      },
+    });
+    return res.status(200).json(`${subject.name} has been deleted!`);
+  });
+};
+
+// METHOD UPDATE SUBJECT
+export const updateSubject = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) {
+    return res.status(401).json("YOU'RE NOT LOGGED IN!");
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, async (err, userInfo) => {
+    if (err) {
+      return res.status(403).json("INVALID TOKEN!");
+    }
+
+    const newName = req.body.name;
+
+    const existingName = await prisma.subject.findUnique({
+      where: {
+        name: newName,
+      },
+    });
+
+    if (existingName) {
+      return res.status(403).json("This subject already exist!");
+    } else {
+      const subjectId = parseInt(req.params.id);
+      await prisma.subject.update({
+        where: {
+          id: subjectId,
+        },
+        data: {
+          name: newName,
+        },
+      });
+
+      return res.status(200).json("Subject updated successfully!");
+    }
   });
 };
--- routes/subjects.js
+++ routes/subjects.js
@@ -1,14 +1,18 @@
 import express from "express";
 import {
   addSubject,
+  deleteSubject,
   getAllSubjects,
   getSubject,
+  updateSubject,
 } from "../controllers/subject.js";
 
 const router = express.Router();
 
 router.get("/", getAllSubjects);
 router.get("/:id", getSubject);
+router.delete("/:id", deleteSubject);
 router.post("/", addSubject);
+router.put("/:id", updateSubject);
 
 export default router;
