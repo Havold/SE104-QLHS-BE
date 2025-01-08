6ec8c6a53fff70bacfb6163995ed1d07af1ef27c
Trương Hữu Thọ <96384986+Havold@users.noreply.github.com>
2025-01-08 18:26:14 +0700

    Add method GET detail score board.

--- controllers/detailScoreBoard.js
+++ controllers/detailScoreBoard.js
@@ -1,6 +1,61 @@
 import prisma from "../client.js";
 import jwt from "jsonwebtoken";
 
+export const getDetailScoreBoard = async (req, res) => {
+  const { page, pageItems, type, ...queryParams } = req.query;
+  let p = page ? parseInt(page) : 1;
+  let pItems = pageItems ? parseInt(pageItems) : 5;
+  let query = { scoreBoardId: parseInt(req.params.id) };
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      switch (key) {
+        case "search":
+          query.name = {
+            contains: value,
+            mode: "insensitive",
+          };
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  try {
+    const count = await prisma.dT_ScoreBoard.count({ where: query });
+    if (p * pItems > count) {
+      p = Math.ceil(count / pItems);
+    }
+    // Nếu type là "all", lấy toàn bộ dữ liệu mà không áp dụng phân trang
+    let dT_ScoreBoards;
+    if (type === "all") {
+      dT_ScoreBoards = await prisma.dT_ScoreBoard.findMany({
+        where: query,
+        orderBy: {
+          ["id"]: "asc", // Sắp xếp theo trường và thứ tự
+        },
+      });
+      res
+        .status(200)
+        .json({ dT_ScoreBoards, totalCount: count, currentPage: 1 });
+      return;
+    }
+
+    if (p <= 0) {
+      p = 1;
+    }
+    dT_ScoreBoards = await prisma.dT_ScoreBoard.findMany({
+      where: query,
+      take: pItems,
+      skip: (p - 1) * pItems,
+    });
+    res.status(200).json({ dT_ScoreBoards, totalCount: count, currentPage: p });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
 export const addStudentsToDTScoreBoard = (req, res) => {
   const token = req.cookies.accessToken;
   if (!token) {
--- routes/detailScoreBoards.js
+++ routes/detailScoreBoards.js
@@ -1,11 +1,13 @@
 import express from "express";
 import {
   addStudentsToDTScoreBoard,
+  getDetailScoreBoard,
   removeStudentFromDTScoreBoard,
 } from "../controllers/detailScoreBoard.js";
 
 const router = express.Router();
 
+router.get("/:id", getDetailScoreBoard);
 router.post("/", addStudentsToDTScoreBoard);
 router.delete("/:id/students/:studentId", removeStudentFromDTScoreBoard);
 
