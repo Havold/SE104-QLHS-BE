3e3cd12cf4eb21ea63e19c497be3baee3fdd820b
Trương Hữu Thọ <96384986+Havold@users.noreply.github.com>
2025-01-28 18:03:07 +0700

    Add error handling if search input is not a number when search grade.

--- controllers/grade.js
+++ controllers/grade.js
@@ -1,47 +1,6 @@
 import prisma from "../client.js";
 import jwt from "jsonwebtoken";
 
-// export const getAllGrades = async (req, res) => {
-//   const { page, pageItems, ...queryParams } = req.query;
-//   let p = page ? parseInt(page) : 1;
-//   let pItems = pageItems ? parseInt(pageItems) : 5;
-//   let query = {};
-//   if (queryParams) {
-//     for (const [key, value] of Object.entries(queryParams)) {
-//       switch (key) {
-//         case "search":
-//           query.level = parseInt(value);
-//           break;
-//         default:
-//           break;
-//       }
-//     }
-//   }
-
-//   try {
-//     const count = await prisma.grade.count({ where: query });
-//     if (p * pItems > count) {
-//       p = Math.ceil(count / pItems);
-//     }
-
-//     if (p <= 0) {
-//       p = 1;
-//     }
-//     const grades = await prisma.grade.findMany({
-//       where: query,
-//       take: pItems,
-//       skip: (p - 1) * pItems,
-//       orderBy: {
-//         ["level"]: "asc", // Sắp xếp theo trường và thứ tự
-//       },
-//     });
-//     res.status(200).json({ grades, totalCount: count, currentPage: p });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json(error);
-//   }
-// };
-
 export const getAllGrades = async (req, res) => {
   const { page, pageItems, type, ...queryParams } = req.query;
   let p = page ? parseInt(page) : 1;
@@ -51,7 +10,17 @@ export const getAllGrades = async (req, res) => {
     for (const [key, value] of Object.entries(queryParams)) {
       switch (key) {
         case "search":
-          query.level = parseInt(value);
+          if (value) {
+            const searchValue = parseInt(value);
+            if (!isNaN(searchValue)) {
+              query.level = {
+                equals: searchValue,
+              };
+            } else {
+              return res.status(500).json("Input search should be a number!");
+            }
+          }
+
           break;
         default:
           break;
