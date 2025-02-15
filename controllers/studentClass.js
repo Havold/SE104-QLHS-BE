2cf19762d905949d7acc0e43ce469b83c2c6e322
Trương Hữu Thọ <96384986+Havold@users.noreply.github.com>
2025-02-15 13:34:42 +0700

    Remove unnecessary things.

--- controllers/studentClass.js
+++ controllers/studentClass.js
@@ -56,61 +56,6 @@ export const getStudentsFromClass = async (req, res) => {
   }
 };
 
-// export const addStudentsToClass = async (req, res) => {
-//   const token = req.cookies.accessToken;
-//   if (!token) {
-//     return res.status(401).json("YOU ARE NOT LOGIN!");
-//   }
-
-//   jwt.verify(token, process.env.JWT_SECRET, async (err, userInfo) => {
-//     if (err) {
-//       return res.status(403).json("INVALID TOKEN!");
-//     }
-
-//     let { classSchoolYearId, studentIds } = req.body;
-//     classSchoolYearId = parseInt(classSchoolYearId);
-
-//     try {
-//       // Kiểm tra lớp học có tồn tại không
-//       const classExists = await prisma.classSchoolYear.findUnique({
-//         where: {
-//           id: classSchoolYearId,
-//         },
-//       });
-
-//       if (!classExists) {
-//         return res.status(404).json("Class not found!");
-//       }
-
-//       // Thêm học sinh vào lớp học sử dụng connect
-//       const studentClassData = studentIds.map((studentId) => ({
-//         studentId: studentId, // Chỉ định studentId trực tiếp
-//         classSchoolYearId: classSchoolYearId, // Chỉ định classSchoolYearId trực tiếp
-//       }));
-
-//       // Sử dụng createMany để thêm nhiều bản ghi vào bảng StudentClass
-//       const addedStudents = await prisma.studentClass.createMany({
-//         data: studentClassData,
-//         skipDuplicates: true, // Bỏ qua nếu học sinh đã được thêm
-//       });
-
-//       // Cập nhật capacity
-//       const currentCapacity = classExists.capacity + studentIds.length;
-//       await prisma.classSchoolYear.update({
-//         where: { id: classSchoolYearId },
-//         data: { capacity: currentCapacity },
-//       });
-
-//       return res
-//         .status(200)
-//         .json("Students have been successfully added to the class!");
-//     } catch (error) {
-//       console.error(error);
-//       return res.status(500).json("Something went wrong!");
-//     }
-//   });
-// };
-
 export const addStudentsToClass = async (req, res) => {
   const token = req.cookies.accessToken;
   if (!token) {
@@ -138,19 +83,40 @@ export const addStudentsToClass = async (req, res) => {
 
       const schoolYearId = classExists.schoolYearId;
 
-      // 2. Thêm học sinh vào lớp học sử dụng connect
+      // 2. Cập nhật capacity của lớp
+      const currentCapacity = classExists.capacity + studentIds.length;
+      const maxCapacity = await prisma.rule.findFirst({
+        where: {
+          name: "Max Capacity",
+        },
+      });
+
+      if (currentCapacity > maxCapacity.value) {
+        return res
+          .status(400)
+          .json(
+            `Max capacity is ${maxCapacity.value}. Current capacity: ${classExists.capacity}`
+          );
+      }
+
+      await prisma.classSchoolYear.update({
+        where: { id: classSchoolYearId },
+        data: { capacity: currentCapacity },
+      });
+
+      // 3. Thêm học sinh vào lớp học sử dụng connect
       const studentClassData = studentIds.map((studentId) => ({
         studentId: studentId, // Chỉ định studentId trực tiếp
         classSchoolYearId: classSchoolYearId, // Chỉ định classSchoolYearId trực tiếp
       }));
 
-      // Sử dụng createMany để thêm nhiều bản ghi vào bảng StudentClass
+      // 4. Sử dụng createMany để thêm nhiều bản ghi vào bảng StudentClass
       const addedStudents = await prisma.studentClass.createMany({
         data: studentClassData,
         skipDuplicates: true, // Bỏ qua nếu học sinh đã được thêm
       });
 
-      // 3. Tạo `Result` cho mỗi học sinh
+      // 5. Tạo `Result` cho mỗi học sinh
       const resultData = studentIds.map((studentId) => ({
         id: `${studentId}_${schoolYearId}`, // Tạo id = studentId + schoolYearId
         studentId: studentId,
@@ -159,17 +125,10 @@ export const addStudentsToClass = async (req, res) => {
         avgSemII: null, // Giá trị mặc định ban đầu
       }));
 
-      // Thêm vào bảng Result
+      // 6. Thêm vào bảng Result
       await prisma.result.createMany({
         data: resultData,
         skipDuplicates: true, // Tránh thêm trùng lặp nếu đã tồn tại
-      });
-
-      // 4. Cập nhật capacity của lớp
-      const currentCapacity = classExists.capacity + studentIds.length;
-      await prisma.classSchoolYear.update({
-        where: { id: classSchoolYearId },
-        data: { capacity: currentCapacity },
       });
 
       return res
